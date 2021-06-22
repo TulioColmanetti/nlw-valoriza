0f3fd73c248afd224c31f8107407a68d59f15658
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-06-21 22:52:26 -0300

    Added comments and TypeORM / PostgreSQL dependencies

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,24 @@ import express from 'express';
 // @types/express
 const app = express();
 
+/**
+ * GET    => Search information
+ * POST   => Insert (create) information
+ * PUT    => Change information
+ * DELETE => Remove information
+ * PATCH  => Change one specific information
+ */
+
+/**
+ * Parameter types
+ * Route Params => http://localhost:3000/produtos/12345678
+ * Query Params => http://localhost:3000/produtos?name=teclado&description=tecladobom
+ * Body Params  => {
+ *  "name": "teclado",
+ *  "description": "teclado bom"
+ * }
+ */
+
 app.get('/test', (req, res) => {
   return res.send('Olá NLW!');
 });
